71a505ee35905bf6871fdd9a3c84bf27cca06029
calxifer <hxneymilk7@gmail.com>
2026-03-24 23:02:51 +0000

    Fixed diaspeed, finished level0 design

--- Assets/Scripts/Interaction/NPCDialogue.cs
+++ Assets/Scripts/Interaction/NPCDialogue.cs
@@ -7,8 +7,8 @@ public class NPCDialogue : ScriptableObject
     public Sprite npcPortrait;
     public string[] dialogueLines;
     public bool[] autoProgressLines;
-    public float autoProgressDelay = 20f;
-    public float typingSpeed = 10000000f;
+    public float autoProgressDelay = 1.5f;
+    public float typingSpeed = 0.05f;
     public AudioClip voiceSound;
     public float voicePitch = 1f;
 
